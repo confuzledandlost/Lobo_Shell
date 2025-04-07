d32528258dfe9a2d63142d320ccf013ce0d45f81
Sean Belingheri <sdbel@comcast.net>
2025-04-07 11:24:04 -0700

    Oh no, we have other problems

--- include/command.h
+++ include/command.h
@@ -11,23 +11,6 @@ struct Command {
 };
 
 // Function to free a single command
-void FreeCommand(struct Command* cmd) {
-    if (cmd == NULL) return;
-
-    // Free the argument vector
-    if (cmd->argv != NULL) {
-        for (int i = 0; cmd->argv[i] != NULL; i++) {
-            free(cmd->argv[i]);
-        }
-        free(cmd->argv);
-    }
-
-    // Free input/output file strings if they exist
-    if (cmd->input_file) free(cmd->input_file);
-    if (cmd->output_file) free(cmd->output_file);
-
-    // Finally free the command structure itself
-    free(cmd);
-}
+void FreeCommand(struct Command* cmd);
 
 #endif 
